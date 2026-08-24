f4f391d6718184904a072d41b4507d46987c5dfd
Phantom <faizanshah2m111@gmail.com>
2026-08-24 19:45:04 +0500

    login redicret

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,29 +1,30 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface props {
   username: string;
   avatar: string;
 }
 
-export default function HeroSection({username, avatar}: props) {
+export default function HeroSection({ username, avatar }: props) {
   return (
     <section className="relative max-w-5xl w-full shrink-0 mx-auto overflow-hidden rounded-3xl p-4 sm:p-6">
       <div className="relative z-10 flex flex-col">
         <div className="flex items-center justify-start gap-2">
-          <div className="flex items-center gap-1 p-1 pr-2 rounded-3xl shadow-sm backdrop-blur-2xl bg-primary-bg/45 ring-1 ring-primary-color/30 hover:bg-slate-50 transition">
-            <div className="relative h-12 w-12 overflow-hidden rounded-full border-2 border-white shadow-sm">
-              <Image
-                src={avatar}
-                alt="User Avatar"
-                width={100}
-                height={100}
-                className="object-cover"
-              />
-            </div>
-            <div>
+          <Link href={"/login"}>
+            <div className="flex items-center gap-1 p-1 pr-2 rounded-3xl shadow-sm backdrop-blur-2xl bg-primary-bg/45 ring-1 ring-primary-color/30 hover:bg-slate-50 transition">
+              <div className="relative h-12 w-12 overflow-hidden rounded-full border-2 border-white shadow-sm">
+                <Image
+                  src={avatar}
+                  alt="User Avatar"
+                  width={100}
+                  height={100}
+                  className="object-cover"
+                />
+              </div>
               <p className="text-lg font-bold text-secondary">{username}</p>
             </div>
-          </div>
+          </Link>
         </div>
 
         <div className="relative flex flex-col items-center justify-between">
