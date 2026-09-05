b3d9c48cecbfa6d96a65204a626da09445515463
Phantom <faizanshah2m111@gmail.com>
2026-09-05 19:06:42 +0500

    Added ads.txt and google adsense header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { baloo2, geistSans } from "./utils/libs/fonts";
 import "./globals.css";
 import { GlowBackground } from "./components/GlowBg";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Rate My Excuse",
@@ -21,6 +22,20 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${baloo2.variable} antialiased`}
     >
+      <head>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2430162464533504"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+        <Script id="ad-config" strategy="afterInteractive">
+          {`
+            window.adsbygoogle = window.adsbygoogle || [];
+            window.adBreak = window.adConfig = function(o) { adsbygoogle.push(o); };
+          `}
+        </Script>
+      </head>
       <body className="h-screen w-screen flex flex-col items-center overflow-y-auto overflow-x-hidden">
         <GlowBackground className="fixed max-w-5xl w-full inset-y-0 -z-10" />
         {children}
